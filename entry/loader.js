0c2ed54966fc188785837b02dc06a2d2f5bdb917
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-04-27 16:29:22 +0400

    Added css to 'web_accessible_resources'

--- entry/loader.js
+++ entry/loader.js
@@ -1,16 +1,36 @@
 (function() {
+    Array.prototype.filterWithExtention = function(fileExtention) {
+        return this.filter(file => file.toLowerCase().endsWith(fileExtention));
+    };
+
+    const url = (res) => chrome.runtime.getURL(res);
+    const createElement = (tagName, attrs = {}) => {
+        const tag = document.createElement(tagName);
+        Object.keys(attrs).forEach(key => {
+            tag.setAttribute(key, attrs[key]);
+        });
+        return tag;
+    };
+
     const manifest = chrome.runtime.getManifest();
     const webResources = manifest.web_accessible_resources || [];
     const resources = webResources.map((obj) => obj["resources"] || []).flat();
-    const jsResources = resources.filter((res) => {
-        return res.toLowerCase().endsWith(".js")
-    });
+
     const head = document.head || document.getElementsByTagName("head")[0];
-    jsResources.forEach(jsRes => {
-        const url = chrome.runtime.getURL(jsRes);
-        const scriptTag = document.createElement("script");
-        scriptTag.setAttribute("type", "module");
-        scriptTag.setAttribute("src", url);
+    resources.filterWithExtention(".js").forEach(res => {
+        const scriptTag = createElement("script", {
+            type: "module",
+            src: url(res),
+        });
         head.insertBefore(scriptTag, head.lastChild);
     });
+    resources.filterWithExtention(".css").forEach(res => {
+        const linkTag = createElement("link", {
+            rel: "stylesheet",
+            href: url(res),
+        });
+        head.insertBefore(linkTag, head.lastChild);
+    });
+
+    Array.prototype.filterWithExtention = undefined;
 })();
