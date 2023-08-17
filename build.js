e37862fabd996e5358e36a9ab8a2c5aea2ed3c4c
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-08-17 21:03:36 +0400

    Improved building

--- build.js
+++ build.js
@@ -1,41 +1,59 @@
 import * as fs from "fs";
 import * as uglifyJS from "uglify-js";
+import cleanCss from 'clean-css';
 import { execSync } from "child_process";
 import { join, dirname } from "path";
 
-
-const USE_LOG = true;
+const CleanCSS = cleanCss;
 
 
 const UTF_8 = "utf8";
-const TAB = new Array(4).fill(" ").join("");
+const TAB = "    ";
 
 const MANIFEST_JSON = "manifest.json";
 const TS_CONFIG_JSON = "tsconfig.json";
 const GITIGNORE_FILE = ".gitignore";
 const ENTRY_JS = "src/entry.js";
-
 const BUILD_DIR = "tomara-extension";
 
+const DO_MINIFY = false;
+const MINIFY_JS_OPTS = {
+    mangle: false,
+    compress: {
+        passes: 3,
+    },
+};
+const MINIFY_CSS_OPTS = {
+    level: {
+        1: {
+            all: true,
+        },
+    },
+};
 
-class FileInfo {
-    constructor(path, doMinify = true) {
-        this.path = path;
-        this.doMinify = doMinify;
-    }
-}
 
 const info = console.log;
-const log = (...data) => {
-    if(USE_LOG) {
-        console.log(...data);
-    }
+const withLog = (func) => {
+    return srcFile => {
+        const newFile = join(BUILD_DIR, srcFile);
+        func(srcFile, newFile);
+        const srcFileSize = fs.statSync(srcFile).size;
+        const newFileSize = fs.statSync(newFile).size;
+        const toLineEnd = (() => {
+            if(!DO_MINIFY || srcFileSize === newFileSize) {
+                return "";
+            }
+            const diff = 1 - newFileSize / srcFileSize;
+            const scaledDiff = Math.round(diff * 100 * 100) / 100;
+            return ` [-${scaledDiff}%]`;
+        })();
+        info(`${TAB}FROM: '${srcFile}'`);
+        info(`${TAB}  TO: '${newFile}${toLineEnd}'`);
+    };
 };
 
 const getField = (config, nestedFieldName) => {
-    const nestedFieldNames = nestedFieldName.split(".")
-        .map((name) => name.trim())
-        .filter((name) => name !== "");
+    const nestedFieldNames = nestedFieldName.split(".");
     let nested = config;
     for(let i = 0; i < nestedFieldNames.length; i++) {
         const fieldName = nestedFieldNames[i];
@@ -66,73 +84,62 @@ const copyFile = (inputFile, outputFile) => {
 
 const minifyJsFile = (inputFile, outputFile) => {
     const inputCode = fs.readFileSync(inputFile, UTF_8);
-    const minifiedCode = uglifyJS.minify(inputCode);
+    const minifiedCode = uglifyJS.minify(inputCode, MINIFY_JS_OPTS);
     createDir(outputFile);
     writeFile(outputFile, minifiedCode.code);
 };
 
-const round = (n) => {
-    const roundRate = 100;
-    return Math.round(n * roundRate) / roundRate;
+const minifyCssFile = (inputFile, outputFile) => {
+    const inputCode = fs.readFileSync(inputFile, UTF_8);
+    const result = new CleanCSS(MINIFY_CSS_OPTS).minify(inputCode);
+    createDir(outputFile);
+    writeFile(outputFile, result.styles);
 };
 
 
 const process = (manifest, tsConfig) => {
-    const FI = (...args) => new FileInfo(...args);
     const filterResources = (resources, fileExtention) => {
         return resources.filter((res) => {
             return res.toLowerCase().endsWith(fileExtention);
         });
     };
 
-    const outDir = getField(tsConfig, "compilerOptions.outDir");
-    const icons = Object.values(getField(manifest, "icons"));
     const webResources = getField(manifest, "web_accessible_resources").map((entry) => {
         return entry["resources"] || [];
     }).flat();
-    const jsResources = filterResources(webResources, ".js").map(res => FI(res, true));
+
+    const jsResources = filterResources(webResources, ".js");
+    const cssResources = filterResources(webResources, ".css");
     const otherResources = (() => {
-        const usedResources = new Set(jsResources);
-        return webResources.filter((res) => {
+        const usedResources = new Set([
+            ...jsResources,
+            ...cssResources,
+        ]);
+        const result = webResources.filter((res) => {
             return !usedResources.has(res);
-        }).map(res => FI(res, false));
+        });
+        result.push(
+            ...Object.values(getField(manifest, "icons"))
+        );
+        result.push(MANIFEST_JSON, ENTRY_JS);
+        return result;
     })();
 
     info("Building...");
     execSync("npm run build");
 
     info("Copying...");
-    let originalSize = 0;
-    let compressedSize = 0;
-    [
-        FI(MANIFEST_JSON, false),
-        FI(ENTRY_JS),
-        ...jsResources,
-        ...otherResources,
-        ...icons,
-    ].forEach((srcFileInfo) => {
-        const srcFile = (srcFileInfo instanceof FileInfo)? srcFileInfo.path: srcFileInfo;
-        const newFile = join(BUILD_DIR, srcFile);
-
-        if(srcFileInfo.doMinify || false) {
-            minifyJsFile(srcFile, newFile);
-            originalSize += fs.statSync(srcFile).size;
-            compressedSize += fs.statSync(newFile).size;
-        } else {
-            copyFile(srcFile, newFile);
-        }
-
-        log(`${TAB}  TO: '${newFile}'`);
-        log(`${TAB}FROM: '${srcFile}'`);
-    });
+    jsResources.forEach(withLog(
+        (DO_MINIFY)? minifyJsFile: copyFile
+    ));
+    cssResources.forEach(withLog(
+        (DO_MINIFY)? minifyCssFile: copyFile
+    ));
+    otherResources.forEach(withLog(copyFile));
 
     writeFile(join(BUILD_DIR, GITIGNORE_FILE), "*\n");
 
-    if(originalSize > 0) {
-        const compressRate = round((1 - (compressedSize / originalSize)) * 100);
-        log(`${TAB}[Compress Rate: ${compressRate}%]`);
-    }
-
+    const outDir = getField(tsConfig, "compilerOptions.outDir");
     info(`Deleting '${outDir}' Directory...`);
     fs.rmSync(outDir, { recursive: true, force: true });
 
